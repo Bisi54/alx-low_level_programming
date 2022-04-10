cb8534440ac0d13e8f757ffd2119dd78e00ff33e
Bisi54 <olabisidosunmu1@gmail.com>
2022-04-10 05:20:06 -0700

    variables-if,else,while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using thee main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	short c;
+
+		for (c = '0'; c <= '9'; c++)
+
+	{
+
+			putchar(c);
+
+	}
+
+	putchar(\n);
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 with formatting using putchar
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int i = 0;
+
+	while (i < 10)
+
+	{
+
+		putchar (i + '0');
+
+		if (i !=9)
+
+	{
+
+		putchar ('0' - 4);
+
+		putchar (' ');
+
+	}
+
+		i++;
+
+	}
+
+		putchar ('\n');
+
+		return (0);
+
+}
