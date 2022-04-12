1c7d66d316fedf9a95e537d2b85ad04c8d842136
Bisi54 <olabisidosunmu1@gmail.com>
2022-04-11 18:30:12 -0700

    print combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints all possible different combination of 2 digits
+ *
+ *Description: using the main function
+ *Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if (f_d < l_d)
+		{
+			putchar(f_d);
+			putchar(l_d);
+
+			if (c != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
